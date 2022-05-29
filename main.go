c299d7b9f0213d7ee7d3c6c599978bce01304a17
lautisaest <lautysaenzestrada@gmail.com>
2022-05-28 21:33:46 -0300

    setting up a hello world server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello, world!")
+		if err != nil {
+			fmt.Println(err)
+
+		}
+		fmt.Println(fmt.Sprintf("Number of ytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
+
 }
