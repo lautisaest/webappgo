f1a6130786e58858a94cea01022ef9a93eedf85a
lautisaest <lautysaenzestrada@gmail.com>
2022-06-14 22:01:50 -0300

    refactoring code to use packages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+
+	handlers "github.com/lautisaest/webappgo/pkg/handlers"
 )
 
 const PortNumer = ":8080"
 
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 	log.Printf("Listening at port %s", PortNumer)
 	_ = http.ListenAndServe(PortNumer, nil)
 
